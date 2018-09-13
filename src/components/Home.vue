926d357e732b8be77d8155804978fb31e9d8bcfc
Nelson Oba <nelsob44@gmail.com>
2018-09-13 11:49:06 +0100

    additions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home"><br>
+  <button style="width:180px; float: right;"><i class="fas fa-shopping-cart"></i>&nbsp;<span>Cart({{cart}})</span></button>
     
     <div class="product" style="margin-top:100px;">
       <h3 class="container">{{ title }}</h3>
@@ -20,13 +21,13 @@
                 <ul>
                   <li v-for="detail in details">{{detail}}</li>
                 </ul>
-                <div v-for="variant in variants" :key="variant.variantId">
+                <div v-for="variant in variants" :key="variant.variantId" class="color-box" :style="{backgroundColor: variant.variantColor}" @mouseover="updateProduct(variant.variantImage)">
                   <p>{{variant.variantColor}}</p>
                 </div>
                 <p><select class="form-control" style="width:100px;">
                   <option v-for="size in sizes">{{size}}</option>
                 </select></p>
-                <button v-on:click="cart +=1" v-if="inventory">Add to Cart</button><button style="width:180px; float: right;"><i class="fas fa-shopping-cart"></i>&nbsp;<span>Cart({{cart}})</span></button>
+                <button v-on:click="addToCart" v-if="inventory > 0" :disabled="inventory < 0">Add to Cart</button><button v-on:click="removeFromCart" v-if="cart > 0" :disabled="inventory < 1">Remove from Cart</button>
                 
               </div>
             </li>
@@ -59,7 +60,8 @@ export default {
       variants: [
         {
           variantId: 2234,
-          variantColor: "green"
+          variantColor: "green",
+          variantImage: [require('@/assets/pictures/flag.png')]
         },
         {
           variantId: 2235,
@@ -92,6 +94,15 @@ export default {
             this.sources = response.data.sources;
             console.log(response.data);
         });
+    },
+    addToCart: function () {
+      this.cart +=1
+    },
+    removeFromCart: function () {
+      this.cart -=1
+    },
+    updateProduct: function (variantImage) {
+      this.product = variantImage
     }
   } 
        
@@ -102,5 +113,8 @@ export default {
 .product-image {
     margin: 30px;
     padding: 30px;
+}
+.color-box {
+  width: 70px;
 }
 </style>
